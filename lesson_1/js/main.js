408ad84e4fb4d60ac8c45e8299110ed37f79873e
Akub1219 <cholebine500@gmail.com>
2021-05-17 22:13:40 +0900

    フィードバック対応（1回目）

--- lesson_1/js/main.js
+++ lesson_1/js/main.js
@@ -8,6 +8,26 @@ $(document).ready(function() {
     Init();
 });
 
+
+
+//ボタンを押す処理はjsにまとめる（htmlに書くとデバッグしづらくなるため）
+$("#init_btn").on("click", function() {
+    Init();
+});
+
+$("#gu_btn").on("click", function() {
+    Janken_btn(1);
+});
+
+$("#cho_btn").on("click", function() {
+    Janken_btn(2);
+});
+$("#pa_btn").on("click", function() {
+    Janken_btn(3);
+});
+
+
+
 //初期化
 function Init() {
     Change_npc_hand(0);
@@ -15,12 +35,13 @@ function Init() {
     Change_call(0);
     Btn_visible(false);
     isPlayable = true;
-    confetti.reset()
+    confetti.reset();
 }
 
 //ボタンの表示非表示
 function Btn_visible(visible) {
-    if (visible == true) {
+    //比較は厳密比較で
+    if (visible === true) {
         document.getElementById("init_btn").style.visibility = "visible";
         isPlayable = false;
     } else {
@@ -29,7 +50,8 @@ function Btn_visible(visible) {
 }
 
 // 自分のじゃんけんの手を出す
-function janken_btn(pc_hand) {
+// 関数名の大文字は共通化
+function Janken_btn(pc_hand) {
     if (isPlayable) {
         // 敵のじゃんけんの手を決める
         var npc_hand = Decide_npc_hand();
@@ -65,19 +87,19 @@ function Judge(pcHand, npcHand) {
     var result = { none: 0, win: 1, lose: 2, aiko: 3 };
     switch (pcHand) {
         case 1:
-            if (npcHand == 1) rResult = result.aiko;
-            else if (npcHand == 2) rResult = result.win;
-            else if (npcHand == 3) rResult = result.lose;
+            if (npcHand === 1) rResult = result.aiko;
+            else if (npcHand === 2) rResult = result.win;
+            else if (npcHand === 3) rResult = result.lose;
             break;
         case 2:
-            if (npcHand == 1) rResult = result.lose;
-            else if (npcHand == 2) rResult = result.aiko;
-            else if (npcHand == 3) rResult = result.win;
+            if (npcHand === 1) rResult = result.lose;
+            else if (npcHand === 2) rResult = result.aiko;
+            else if (npcHand === 3) rResult = result.win;
             break;
         case 3:
-            if (npcHand == 1) rResult = result.win;
-            else if (npcHand == 2) rResult = result.lose;
-            else if (npcHand == 3) rResult = result.aiko;
+            if (npcHand === 1) rResult = result.win;
+            else if (npcHand === 2) rResult = result.lose;
+            else if (npcHand === 3) rResult = result.aiko;
             break;
         default:
             rResult = result.none;
@@ -92,6 +114,7 @@ function Change_npc_face(result_num) {
     document.getElementById("npc_face").src = result_pics_src[result_num];
     console.log("npc_face:" + result_num);
 }
+
 // コール表示を変える
 function Change_call(result_num) {
     var result_call = new Array("手をえらぶニャ", "きみのかち", "ネコのかち", "あいこ");
@@ -99,47 +122,58 @@ function Change_call(result_num) {
     console.log("result_call:" + result_num);
     if (result_num == 1) {
         //勝ち
-        //紙吹雪演出
-        var count = 200;
-        var defaults = {
-            origin: { y: 0.7 }
-        };
+        //単一責任の原則にのっとって外に出す
+        Hanahubuki();
 
-        function fire(particleRatio, opts) {
-            confetti(Object.assign({}, defaults, opts, {
-                particleCount: Math.floor(count * particleRatio)
-            }));
-        }
-
-        fire(0.25, {
-            spread: 26,
-            startVelocity: 55,
-        });
-        fire(0.2, {
-            spread: 60,
-        });
-        fire(0.35, {
-            spread: 100,
-            decay: 0.91,
-            scalar: 0.8
-        });
-        fire(0.1, {
-            spread: 120,
-            startVelocity: 25,
-            decay: 0.92,
-            scalar: 1.2
-        });
-        fire(0.1, {
-            spread: 120,
-            startVelocity: 45,
-        });
-        Btn_visible(true);
     } else if (result_num == 2) {
         //負け
-
         Btn_visible(true);
     }
 }
+
+
+function Hanahubuki() {
+
+    //勝ち
+    //紙吹雪演出
+    var count = 200;
+    var defaults = {
+        origin: { y: 0.7 }
+    };
+
+    function fire(particleRatio, opts) {
+        confetti(Object.assign({}, defaults, opts, {
+            particleCount: Math.floor(count * particleRatio)
+        }));
+    }
+
+    fire(0.25, {
+        spread: 26,
+        startVelocity: 55,
+    });
+    fire(0.2, {
+        spread: 60,
+    });
+    fire(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8
+    });
+    fire(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2
+    });
+    fire(0.1, {
+        spread: 120,
+        startVelocity: 45,
+    });
+    Btn_visible(true);
+}
+
+
+
 
 /*---------------------
     汎用関数
