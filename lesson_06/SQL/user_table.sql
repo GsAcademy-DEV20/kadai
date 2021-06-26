b0b33fde3083f4283c517f9646744e474602b339
Akub1219 <cholebine500@gmail.com>
2021-06-26 22:55:15 +0900

    SQLをアップ

---
+++ lesson_06/SQL/user_table.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2021-06-26 15:52:24
+-- サーバのバージョン： 10.4.19-MariaDB
+-- PHP のバージョン: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `gs_kadai_06`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `user_table`
+--
+
+CREATE TABLE `user_table` (
+  `id` int(12) NOT NULL,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `birthday` date NOT NULL,
+  `avatar` int(12) NOT NULL,
+  `thanks` int(12) NOT NULL,
+  `indate` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `user_table`
+--
+
+INSERT INTO `user_table` (`id`, `name`, `birthday`, `avatar`, `thanks`, `indate`) VALUES
+(1, '111', '2021-04-07', 2, 0, '2021-06-24 17:09:16'),
+(2, 'たろう', '2021-06-05', 1, 0, '2021-06-24 17:47:21');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `user_table`
+--
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `user_table`
+--
+ALTER TABLE `user_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
